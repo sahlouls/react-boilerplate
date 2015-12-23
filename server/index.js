ef6930ee7a9bed67185cdf512cbdd4f895fa746a
Sahbi Sahloul <sahbi.sahloul@amadeus.com>
2015-12-24 00:34:06 +0100

    Initializing server with socket.io and integrating with the redux store

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 import makeStore from './src/store';
+import startServer from './src/server';
+import {
+  SET_ENTRIES, NEXT
+}
+from './src/actions';
 
 export const store = makeStore();
+startServer(store);
+
+store.dispatch({
+  type: SET_ENTRIES,
+  entries: require('./entries.json')
+});
+console.log('\nStore initialized', store.getState());
+
+store.dispatch({
+  type: NEXT
+});
+console.log('\nVote started', store.getState());
